5032caa4ceb1b889423aa9b856d27e384b573401
Charles-Axel Dein <ca@d3in.org>
2012-09-12 17:36:56 +0200

    First commit

---
+++ xunit2csv/xunit2csv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+"""
+Extract test name and time from xunit output.
+"""
+
+import argparse
+import csv
+import xml.etree.ElementTree as etree
+
+def main(args):
+    tree = etree.parse(args.input[0])
+
+    output = []
+    for testcase in tree.iterfind("testcase"):
+        row = {
+                "classname": testcase.get("classname"),
+                "name": testcase.get("name"),
+                "time": testcase.get("time")}
+        output.append(row)
+
+    with open(args.output[0], "w") as f:
+        writer = csv.DictWriter(f, ("classname", "name", "time"))
+        writer.writeheader()
+        writer.writerows(output)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Convert xunit file to csv.")
+    parser.add_argument("input", nargs=1, help="xunit file")
+    parser.add_argument("output", nargs=1, help="csv file")
+    args = parser.parse_args()
+    main(args)
